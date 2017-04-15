0d2a2a645e8c4f914fe541d8755f0b006f1bca35
Leila Compaore <wendsomleila@gmail.com>
2017-04-15 12:29:51 -0400

    token verif

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', function(req, res) {
   res.send('allo le monde');
 })
 
+//MESSENGER PAGE TOKEN VERIFICATION
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'PRINCESSE_YENNENGA') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('are you trynna fool me?? Your token is not working')
+  }
+});
+
 app.listen(app.get('port'), function() {
   console.log("sooo, we're listening fam, check port: "+app.get('port'));
 });
