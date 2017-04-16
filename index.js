78e367a4ebea4d244b5a62dcb49454a638bf3a66
Leila Compaore <wendsomleila@gmail.com>
2017-04-16 00:15:38 -0400

    added punchin and out

--- index.js
+++ index.js
@@ -28,18 +28,33 @@ app.get('/webhook/', function(req, res) {
 
 app.post('/webhook/', function (req, res) {
 
-    var event = req.body.entry[0].messaging[0]
-    var senderID = event.sender.id;
-        if (event.message && event.message.text) {
-          replyfunction(senderID,"you: "+ event.message.text);
-        } else {
-          console.log("AAA");
-          console.log("Webhook received unknown event: ", event);
-          console.log("BBB");
+  var event = req.body.entry[0].messaging[0]
+  var senderID = event.sender.id;
+      if (event.message && event.message.text) {
+        replyfunction(senderID,"you: "+ event.message.text);
+
+        switch (event.message.text) {
+          //punch in
+          case 'pin':
+            punch(senderID, 'punchin');
+            break;
+          //punch out
+          case 'pout':
+              punch(senderID, 'punchout');
+              break;
+          default:
+            replyfunction(senderID,"echo: "+ event.message.text);
         }
 
-        //the following will mess up with your brain for hours if missing
-        res.sendStatus(200)
+
+      } else {
+        console.log("AAA");
+        console.log("Webhook received unknown event: ", event);
+        console.log("BBB");
+      }
+
+      //the following will mess up with your brain for hours if missing
+      res.sendStatus(200)
 
 });
 
@@ -62,4 +77,35 @@ function replyfunction (senderID, text){
               console.log('Error: ', response.body.error)
           }
       })
+}
+
+
+function punch (senderID, text) {
+
+  let message = {text: "not punch in nor punch out"}
+  switch (text) {
+    case "punchin":
+      message.text = "Welcome to the office";
+      break;
+    case "punchout":
+      message.text = "Bye, see you!";
+      break;
+  }
+
+  request ({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:pageToken},
+    method: 'POST',
+    json: {
+      recipient: {id: senderID},
+      message: message
+    }
+  }, function (err, res, body) {
+    if(err) {
+      console.log('Error sending messages (punch()): ', err)
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+    }
+  }
+  });
 }
